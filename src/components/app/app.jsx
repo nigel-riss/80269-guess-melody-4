ebce92145b98712130d3d7341933b539aa33c350
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-07-02 13:07:48 +0200

    3.1. Add router. Fix minor bugs

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,52 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-// import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
+import {GameType} from '../../const.js';
 
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
 
+    this.state = {
+      step: -1,
+    };
+
     this._handleWelcomeButtonClick = this._handleWelcomeButtonClick.bind(this);
   }
 
   render() {
-    const {errorCount} = this.props;
+    const {questions} = this.props;
 
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/dev-artist">
+            <ArtistQuestionScreen
+              onAnswer={() => {}}
+              question={questions[1]}
+            />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GenreQuestionScreen
+              onAnswer={() => {}}
+              question={questions[0]}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _renderGameScreen() {
     return <WelcomeScreen
-      errorCount={errorCount}
+      errorCount={13}
       onWelcomeButtonClick={this._handleWelcomeButtonClick}
     />;
   }
