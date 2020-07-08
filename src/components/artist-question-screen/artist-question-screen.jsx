cbaf48c6161228b6a739883db22c18a92003e3b1
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-07-08 17:32:28 +0200

    4.1. Add AudioPlayer component

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const.js';
+import AudioPlayer from '../audio-player/audio-player.jsx';
 
 
 class ArtistQuestionScreen extends PureComponent {
@@ -17,12 +18,10 @@ class ArtistQuestionScreen extends PureComponent {
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
           <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio
-                src={song.src}
-              />
-            </div>
+            <AudioPlayer
+              isPlaying={true}
+              src={song.src}
+            />
           </div>
         </div>
 
--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const.js';
+import AudioPlayer from '../audio-player/audio-player.jsx';
 
 
 class GenreQuestionScreen extends PureComponent {
@@ -29,12 +30,10 @@ class GenreQuestionScreen extends PureComponent {
         >
           {answers.map((answer, i) => (
             <div key={`${i}-${answer.src}`} className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio
-                  src={answer.src}
-                />
-              </div>
+              <AudioPlayer
+                isPlaying={i === 0}
+                src={answer.src}
+              />
               <div className="game__answer">
                 <input
                   className="game__input visually-hidden"
