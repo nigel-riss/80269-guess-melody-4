9d3133e373dafa0c83e8b8ae477d6f213e6e6db8
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-08-04 16:58:12 +0200

    7.1. Decompose reducer

---
+++ src/reducer/user/user.js
@@ -0,0 +1,38 @@
+import {extend} from '../utils.js';
+
+
+const AuthorizationStatus = {
+  NO_AUTH: `NO_AUTH`,
+  AUTH: `AUTH`,
+};
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRE_AUTHORIZATION: `REQUIRE_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRE_AUTHORIZATION,
+      payload: status,
+    };
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRE_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+
+export {reducer, ActionType, ActionCreator};
